514d4095b372339d71ffbcff3cc1422eb65f7f18
sagarhani <sagarhani33@gmail.com>
2019-01-27 23:47:56 +0530

    Simulating change events

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { mount } from 'enzyme';
-import CommentBox from 'components/CommentBox';
+import CommentBox from '../CommentBox';
 
 let wrapped; 
 
@@ -16,4 +16,12 @@ it('has a text area and a button', () => {
 
     expect(wrapped.find('textarea').length).toEqual(1);
     expect(wrapped.find('button').length).toEqual(1);
+});
+
+it('has a text area that users can type in',  () => {
+    wrapped.find('textarea').simulate('change', {
+        target: { value: 'new comment'}
+    });
+    wrapped.update();
+    expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
 });
