3deedab9836b02244bb6cfad2b555ed9a15b7e97
sagarhani <sagarhani33@gmail.com>
2019-02-10 22:29:01 +0530

    Form submit test

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -20,8 +20,21 @@ it('has a text area and a button', () => {
 
 it('has a text area that users can type in',  () => {
     wrapped.find('textarea').simulate('change', {
-        target: { value: 'new comment'}
+        target: { value: 'new comment' }
     });
     wrapped.update();
+
     expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+});
+
+if('when form is submitted, text area gets emptied', () => {
+    wrapped.find('textarea').simulate('change', {
+        target: { value: 'new comment' }
+    });
+    wrapped.update();
+
+    wrapped.find('form').simulate('submit');
+    wrapped.update();
+
+    expect(wrapped.find('textarea').prop('value')).toEqual('');
 });
